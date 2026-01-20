e1592361726d7cafa39aca6eed54f654d2aa4d54
OSM-Made <oldskoolmodz@gmail.com>
2026-01-19 21:05:46 -0700

    fix include dir.

--- sdk.mk
+++ sdk.mk
@@ -4,13 +4,13 @@ OBJCOPY   := objcopy
 STRIP     := strip
 ODIR      := build
 SDIR      := source
+IDIR	  := include
 
 SDK_INCLUDE := $(SDK_ROOT)/include
 SDK_LIB     := $(SDK_ROOT)/lib
 SDK_LD      := $(SDK_ROOT)/ld
 
-RI        := $(shell find $(SDK_INCLUDE) -type d -printf '-I%p ')
-IDIRS     := -I$(SDK_INCLUDE) $(RI)
+IDIRS     := $(shell find $(SDK_INCLUDE) -type d -printf '-I%p ') $(shell find $(IDIR) -type d -printf '-I%p ')
 
 CFLAGS    := $(IDIRS) -O2 -s -w -std=gnu++11 -ffunction-sections -fdata-sections -fno-builtin -fno-exceptions -fno-asynchronous-unwind-tables -nostdlib -w -masm=intel -march=btver2 -m64 -mabi=sysv -mcmodel=small -mstackrealign -D_KERNEL
 
